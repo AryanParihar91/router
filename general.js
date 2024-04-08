db0687e2d18ae82073e4b9acfa19033f69ea45ce
AryanParihar91 <141989485+AryanParihar91@users.noreply.github.com>
2024-04-09 01:17:56 +0530

    Create general.js

---
+++ general.js
@@ -0,0 +1,16 @@
+const express = require('express');
+let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+const public_users = express.Router();
+
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
